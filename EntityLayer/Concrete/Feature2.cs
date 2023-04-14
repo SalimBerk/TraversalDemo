4287b66ef1f341869f7089d2754eaaa3ca7ecf3e
SalimBerk <77536512+SalimBerk@users.noreply.github.com>
2023-04-14 16:12:59 +0300

    16.12
    14.04.2023

---
+++ EntityLayer/Concrete/Feature2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature2
+    {
+        [Key]
+        public int Feature2Id { get; set; }
+        public string Post2Name { get; set; }
+        public string Post2Description { get; set; }
+        public string Post2Image { get; set; }
+        public bool Status2 { get; set; }
+    }
+}
